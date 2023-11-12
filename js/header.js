508786a1f2a550baedd72e252fb2876aad9bea6f
kanor2022 <107674670+kanor2022@users.noreply.github.com>
2023-11-12 09:52:02 +0800

    sepolia

--- js/header.js
+++ js/header.js
@@ -8,7 +8,7 @@ let _CONTRACT_DATA = {};
 const _API_URL = 'https://7yjhmmd390.execute-api.ap-south-1.amazonaws.com/prod'
 
 _CONTRACT_DATA[_NETWORK_ID] = {
-    network_name: "Scroll Testnet",
+    network_name: "Scroll Sepolia",
     explorer_url: 'https://sepolia.scrollscan.com',
     STACKING: {
         sevenDays: {
